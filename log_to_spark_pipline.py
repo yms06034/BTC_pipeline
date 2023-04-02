57a7b9850041817142e92a57f81a3cea7400c7ed
yms06034 <yms06034@gmail.com>
2023-04-02 15:33:54 +0900

    Add

--- log_to_spark_pipline.py
+++ log_to_spark_pipline.py
@@ -1,9 +1,17 @@
 from airflow import DAG
 from airflow.providers.mysql.operators.mysql import MySqlOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 from datetime import datetime
-import pymysql
+
+def upload_to_s3(filename: str, key: str, bucket_name: str) -> None:
+    hook = S3Hook('aws_default')
+    hook.load_file(filename=filename, key=key, bucket_name=bucket_name)
+
+
 
 default_args = {
     'start_date' : datetime(2023, 1, 1)
@@ -16,9 +24,24 @@ with DAG (
     default_args = default_args
 ) as dag:
 
-    spark_to_s3_load = MySqlOperator()
+    # Preprocess
+    load_to_s3 = SparkSubmitOperator(
+        application = "/home/sungjin/airflow/dags/spark_sql_pipeline.py",
+        task_id = "preprocess",
+        conn_id = "spark_local"
+    )
 
-    # Spark에서 정제
+    # Upload_to_S3
+    upload_S3 = PythonOperator(
+        task_id = "upload_S3",
+        python_callable = upload_to_s3,
+        op_kwargs = {
+            'filename' : '/home/sungjin/airflow/data/{0}.parquet',
+            'key' : 'data/log.parquet',
+            'bucket_name' : 'log-to-spark'
+        }
+    )
 
-    # MySql에 담아주기
-    pass
+
+
+    load_to_s3 >> upload_S3
--- spark_sql_pipeline.py
+++ spark_sql_pipeline.py
@@ -1,16 +1,24 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
+from datetime import datetime
 import os
+
+suffix = datetime.now().strftime("%y%m%d")
+
+MAX_MENORY = '5g'
 
 conf = SparkConf()
 
-conf.set("spark.hadoop.fs.s3a.access.key", "AKIAZ5URTMMXVPE4ZB55")
-conf.set("spark.hadoop.fs.s3a.secret.key", "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")
-
-conf.set("spatk.hadoop.fs.s3a.endpoint", "s3.ap-northeast-2.amazonaws.com")
-
+conf.set("spark.hadoop.fs.s3a.access.key", "CKIAD5URTWQXVPE4ZB55")
+conf.set("spark.hadoop.fs.s3a.secret.key", "TdwjfDJsd2ft3ax/T2difLawdhuiTQvb05SKjdwijskdal")
+conf.set("spark.hadoop.fs.s3a.endpoint", "s3.ap-northeast-2.amazonaws.com")
 conf.set('spark.hadoop.fs.s3a.aws.credentials.provider',
          'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
+
+conf.set("spark.executor.memory", MAX_MENORY)
+conf.set("spark.driver.memory", MAX_MENORY)
+conf.set("spark.jars", "/home/yms06034/spark/spark3/mysql-connector-java-8.0.29.jar")
+
 
 spark = SparkSession.builder.config(conf=conf).appName("log_to_spark").getOrCreate()
 
@@ -20,4 +28,23 @@ spark.sparkContext.setSystemProperty("com.amazonaws.services.s3.enableV4", "true
 log_df = spark.read.format("csv") \
             .option("header", "true") \
             .option('infreSchema', 'true') \
-            .load("s3a://depipe/logfile.csv")
+            .load("s3a://log-to-spark/{0}.csv")
+
+log_df.createOrReplaceTempView("log")
+
+query = """
+SELECT 
+  *
+FROM 
+  log
+"""
+
+log_dd = spark.sql(query)
+save_dir = "/home/sungjin/airflow/data/"
+
+log_dd.write.format("parquet").mode("overwrite").save(f"{save_dir}/log_{suffix}/")
+
+log_dd.select("*").write.format("jdbc").mode(saveMode="org.apache.spark.sql.SaveMode.Append") \
+      .option("driver", "com.mysql.jdbc.Driver") \
+      .option("user", "root").option("password", "") \
+      .jdbc(f"jdbc:mysql://localhost:3306/spark_db", "log_{suffix}")
